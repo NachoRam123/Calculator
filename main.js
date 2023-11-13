c858f977ad4324159594e69c25fbcdee09e5668a
NachoRam123 <nacho_ramella@hotmail.com>
2023-11-13 19:01:00 -0300

    adecuate simple math op

--- main.js
+++ main.js
@@ -6,16 +6,20 @@ const subtract = function(num1, num2) {
 	return num1 - num2;
 };
 
-// const multiply = function(arr) {
-//   return arr.reduce((prod, current) => prod * current,1);
-// };
+const multiply = function(num1, num2) {
+  return num1 * num2;
+};
+
+const divide = function(num1, num2) {
+    return num1 / num2;
+} 
 
 const power = function(num1, num2) {
 	return num1 ** num2;
 };
 
-const factorial = function(num1) {
-	let arr = Array.from(Array(num1+1).keys());
-    arr.splice(0,1);
-    return fact =  multiply(arr);
-};
+// const factorial = function(num1) {
+// 	let arr = Array.from(Array(num1+1).keys());
+//     arr.splice(0,1);
+//     return fact =  multiply(arr);
+// };
